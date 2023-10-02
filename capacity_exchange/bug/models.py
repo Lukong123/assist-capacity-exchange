2857cf536bcb695bea75cc34a5539b6f35f6179c
anne <annemachinda@gmail.com>
2023-10-02 22:01:44 +0100

    created Bug model and recorded a bug on django admin

--- capacity_exchange/bug/models.py
+++ capacity_exchange/bug/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Bug(models.Model):
+    description = models.CharField(max_length=200)
+    bug_type = models.CharField(max_length=200)
+    report_date = models.DateTimeField("report date")
+    status = models.CharField(max_length=200)
+    # description", "bug_type", "report_date", "status"
